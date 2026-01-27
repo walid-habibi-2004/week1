88c8574ee9fc9c55f1d4578f082bf5ac4395a7a0
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-01-27 21:17:04 +0100

    Update challenges.js

--- day_1/daily_challenge/challenges.js
+++ day_1/daily_challenge/challenges.js
@@ -1,5 +1,27 @@
 // ====== Daily Challenge 1
 
+let sentence = "The movie is not that bad, I like it"
+
+let wordNot = sentence.indexOf("not")
+let wordBad = sentence.indexOf("bad");
+
+if (wordNot !== -1 && wordBad !== -1 && wordBad > wordNot) {
+    let result = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+    console.log(result);
+} else {
+    console.log(sentence);
+}
+
 // ====== Daily Challenge 2
 
+let n = 6
+for(let i = 1; i <= n; i++){
+    let line = ""
+    for(let j = 1; j <= i; j++){
+        line += "* "
+    }
+    console.log(line)
+}
+
 // ====== Daily Challenge 3
+

975041d102aaeccf448ebcf28c145cc3c6ad6119
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-01-28 20:12:03 +0100

    Update challenges.js

--- day_2/daily_challenge/challenges.js
+++ day_2/daily_challenge/challenges.js
@@ -1,5 +1,39 @@
 // ====== Daily Challenge 1
 
+const planets = [
+  { name: "Mercury", color: "gray", moons: 0 },
+  { name: "Venus", color: "orange", moons: 0 },
+  { name: "Earth", color: "blue", moons: 1 },
+  { name: "Mars", color: "red", moons: 2 },
+  { name: "Jupiter", color: "brown", moons: 79 },
+  { name: "Saturn", color: "goldenrod", moons: 83 },
+  { name: "Uranus", color: "lightblue", moons: 27 },
+  { name: "Neptune", color: "darkblue", moons: 14 }
+];
+
+const section = document.querySelector("section")
+
+planets.forEach((planet) => {
+    const planetDiv = document.createElement("div")
+    planetDiv.classList.add("planet")
+
+    planetDiv.style.backgroundColor = planet.color
+
+    for (let i = 0; i < planet.moons; i++){
+        const moon = document.createElement("div")
+        moon.classList.add("moon")
+
+        moon.style.backgroundColor = "white"
+
+        planetDiv.appendChild(moon)
+    }
+
+    section.appendChild(planetDiv)
+})
+
+console.log(section)
+
 // ====== Daily Challenge 2
 
 // ====== Daily Challenge 3
+

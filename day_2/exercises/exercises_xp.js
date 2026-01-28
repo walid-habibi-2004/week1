650ae4f310befe7e4afac2f5751e867b003aa054
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-01-28 13:44:24 +0100

    Update exercises_xp.js

--- day_2/exercises/exercises_xp.js
+++ day_2/exercises/exercises_xp.js
@@ -221,3 +221,43 @@ console.log(firstLi.textContent);
 console.log(lastLi.textContent);
 
 
+// ===== Exercise 7
+
+const allBooks = [
+    {
+        title: "Harry Potter",
+        author: "J.K. Rowling",
+        image: "https://covers.openlibrary.org/b/id/7984916-L.jpg",
+        alreadyRead: true
+    },
+    {
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        image: "https://covers.openlibrary.org/b/id/8278326-L.jpg",
+        alreadyRead: false
+    }
+];
+
+const section = document.querySelector(".listBooks")
+
+for (let book of allBooks) {
+    const div = document.createElement("div")
+
+    const p = document.createElement("p")
+    p.textContent = `${book.title}  written by ${book.author}`
+
+    const img = document.createElement("img")
+    img.src = book.image
+    img.style.width = "100px"
+
+
+    if(book.alreadyRead === true)
+        p.style.color = "red"
+
+    div.appendChild(p)
+    div.appendChild(img)
+    section.appendChild(div)
+}
+
+
+
